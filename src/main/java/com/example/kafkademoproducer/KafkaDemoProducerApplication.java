1a33675cca09e7264a879277ac6cfe4058a6fdda
jeroen.degroot <jeroen.degroot@edsn.nl>
2023-02-16 15:54:02 +0100

    unused import

--- src/main/java/com/example/kafkademoproducer/KafkaDemoProducerApplication.java
+++ src/main/java/com/example/kafkademoproducer/KafkaDemoProducerApplication.java
@@ -2,7 +2,6 @@ package com.example.kafkademoproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @SpringBootApplication
 public class KafkaDemoProducerApplication {
